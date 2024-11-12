0404e4fed0da6f9f78499de7de89bc01e44678eb
Fabrizio Sestito <fabrizio.sestito@suse.com>
2024-11-12 08:15:16 +0100

    docs: add docstrings to ImageMetadata
    Signed-off-by: Fabrizio Sestito <fabrizio.sestito@suse.com>

--- api/storage/v1alpha1/image_metadata.go
+++ api/storage/v1alpha1/image_metadata.go
@@ -1,11 +1,17 @@
 package v1alpha1
 
+// ImageMetadata contains the metadata details of an image.
 type ImageMetadata struct {
-	Registry   string `json:"registry"`
+	// Registry specifies the name of the Registry object in the same namespace where the image is stored.
+	Registry string `json:"registry"`
+	// Repository specifies the repository path of the image. Example: "rancher/sbombastic".
 	Repository string `json:"repository"`
-	Tag        string `json:"tag"`
-	Platform   string `json:"platform"`
-	Digest     string `json:"digest"`
+	// Tag specifies the tag of the image. Example: "latest".
+	Tag string `json:"tag"`
+	// Platform specifies the platform of the image. Example "linux/amd64".
+	Platform string `json:"platform"`
+	// Digest specifies the sha256 digest of the image.
+	Digest string `json:"digest"`
 }
 
 type ImageMetadataAccessor interface {
