3ef10ecc64ffe24806394c9a41ecbe451bd9d4be
Fabrizio Sestito <fabrizio.sestito@suse.com>
2024-11-28 18:58:48 +0100

    fix(storage): selectable fields were wrong
    Signed-off-by: Fabrizio Sestito <fabrizio.sestito@suse.com>

--- internal/storage/matcher.go
+++ internal/storage/matcher.go
@@ -40,12 +40,12 @@ func getAttrs(obj runtime.Object) (labels.Set, fields.Set, error) {
 	}
 
 	selectableFields := fields.Set{
-		"spec.imageMetadata.Registry":    imageMetadataAccessor.GetImageMetadata().Registry,
-		"spec.imageMetadata.RegistryURI": imageMetadataAccessor.GetImageMetadata().RegistryURI,
-		"spec.imageMetadata.Repository":  imageMetadataAccessor.GetImageMetadata().Repository,
-		"spec.imageMetadata.Tag":         imageMetadataAccessor.GetImageMetadata().Tag,
-		"spec.imageMetadata.Platform":    imageMetadataAccessor.GetImageMetadata().Platform,
-		"spec.imageMetadata.Digest":      imageMetadataAccessor.GetImageMetadata().Digest,
+		"spec.imageMetadata.registry":    imageMetadataAccessor.GetImageMetadata().Registry,
+		"spec.imageMetadata.registryURI": imageMetadataAccessor.GetImageMetadata().RegistryURI,
+		"spec.imageMetadata.repository":  imageMetadataAccessor.GetImageMetadata().Repository,
+		"spec.imageMetadata.tag":         imageMetadataAccessor.GetImageMetadata().Tag,
+		"spec.imageMetadata.platform":    imageMetadataAccessor.GetImageMetadata().Platform,
+		"spec.imageMetadata.digest":      imageMetadataAccessor.GetImageMetadata().Digest,
 	}
 
 	return labels.Set(objMeta.GetLabels()), generic.MergeFieldsSets(selectableMetadata, selectableFields), nil
