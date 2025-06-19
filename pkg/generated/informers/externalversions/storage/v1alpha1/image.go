4e7a08d33e4719c9f0bb9f8403d5411852f16ef6
Fabrizio Sestito <fabrizio.sestito@suse.com>
2025-06-19 12:34:17 +0200

    chore: run make generate
    Signed-off-by: Fabrizio Sestito <fabrizio.sestito@suse.com>

--- pkg/generated/informers/externalversions/storage/v1alpha1/image.go
+++ pkg/generated/informers/externalversions/storage/v1alpha1/image.go
@@ -61,13 +61,25 @@ func NewFilteredImageInformer(client versioned.Interface, namespace string, resy
 				if tweakListOptions != nil {
 					tweakListOptions(&options)
 				}
-				return client.StorageV1alpha1().Images(namespace).List(context.TODO(), options)
+				return client.StorageV1alpha1().Images(namespace).List(context.Background(), options)
 			},
 			WatchFunc: func(options v1.ListOptions) (watch.Interface, error) {
 				if tweakListOptions != nil {
 					tweakListOptions(&options)
 				}
-				return client.StorageV1alpha1().Images(namespace).Watch(context.TODO(), options)
+				return client.StorageV1alpha1().Images(namespace).Watch(context.Background(), options)
+			},
+			ListWithContextFunc: func(ctx context.Context, options v1.ListOptions) (runtime.Object, error) {
+				if tweakListOptions != nil {
+					tweakListOptions(&options)
+				}
+				return client.StorageV1alpha1().Images(namespace).List(ctx, options)
+			},
+			WatchFuncWithContext: func(ctx context.Context, options v1.ListOptions) (watch.Interface, error) {
+				if tweakListOptions != nil {
+					tweakListOptions(&options)
+				}
+				return client.StorageV1alpha1().Images(namespace).Watch(ctx, options)
 			},
 		},
 		&apistoragev1alpha1.Image{},
