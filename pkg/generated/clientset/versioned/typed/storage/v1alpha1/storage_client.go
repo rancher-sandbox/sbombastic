a7fa4fec9e41d23226175308ce2885ee81b8f9cd
Fabrizio Sestito <fabrizio.sestito@suse.com>
2025-06-19 12:34:17 +0200

    chore: run make generate
    Signed-off-by: Fabrizio Sestito <fabrizio.sestito@suse.com>

--- pkg/generated/clientset/versioned/typed/storage/v1alpha1/storage_client.go
+++ pkg/generated/clientset/versioned/typed/storage/v1alpha1/storage_client.go
@@ -54,9 +54,7 @@ func (c *StorageV1alpha1Client) VulnerabilityReports(namespace string) Vulnerabi
 // where httpClient was generated with rest.HTTPClientFor(c).
 func NewForConfig(c *rest.Config) (*StorageV1alpha1Client, error) {
 	config := *c
-	if err := setConfigDefaults(&config); err != nil {
-		return nil, err
-	}
+	setConfigDefaults(&config)
 	httpClient, err := rest.HTTPClientFor(&config)
 	if err != nil {
 		return nil, err
@@ -68,9 +66,7 @@ func NewForConfig(c *rest.Config) (*StorageV1alpha1Client, error) {
 // Note the http client provided takes precedence over the configured transport values.
 func NewForConfigAndClient(c *rest.Config, h *http.Client) (*StorageV1alpha1Client, error) {
 	config := *c
-	if err := setConfigDefaults(&config); err != nil {
-		return nil, err
-	}
+	setConfigDefaults(&config)
 	client, err := rest.RESTClientForConfigAndClient(&config, h)
 	if err != nil {
 		return nil, err
@@ -93,7 +89,7 @@ func New(c rest.Interface) *StorageV1alpha1Client {
 	return &StorageV1alpha1Client{c}
 }
 
-func setConfigDefaults(config *rest.Config) error {
+func setConfigDefaults(config *rest.Config) {
 	gv := storagev1alpha1.SchemeGroupVersion
 	config.GroupVersion = &gv
 	config.APIPath = "/apis"
@@ -102,8 +98,6 @@ func setConfigDefaults(config *rest.Config) error {
 	if config.UserAgent == "" {
 		config.UserAgent = rest.DefaultKubernetesUserAgent()
 	}
-
-	return nil
 }
 
 // RESTClient returns a RESTClient that is used to communicate
--- pkg/generated/informers/externalversions/storage/v1alpha1/sbom.go
+++ pkg/generated/informers/externalversions/storage/v1alpha1/sbom.go
@@ -61,13 +61,25 @@ func NewFilteredSBOMInformer(client versioned.Interface, namespace string, resyn
 				if tweakListOptions != nil {
 					tweakListOptions(&options)
 				}
-				return client.StorageV1alpha1().SBOMs(namespace).List(context.TODO(), options)
+				return client.StorageV1alpha1().SBOMs(namespace).List(context.Background(), options)
 			},
 			WatchFunc: func(options v1.ListOptions) (watch.Interface, error) {
 				if tweakListOptions != nil {
 					tweakListOptions(&options)
 				}
-				return client.StorageV1alpha1().SBOMs(namespace).Watch(context.TODO(), options)
+				return client.StorageV1alpha1().SBOMs(namespace).Watch(context.Background(), options)
+			},
+			ListWithContextFunc: func(ctx context.Context, options v1.ListOptions) (runtime.Object, error) {
+				if tweakListOptions != nil {
+					tweakListOptions(&options)
+				}
+				return client.StorageV1alpha1().SBOMs(namespace).List(ctx, options)
+			},
+			WatchFuncWithContext: func(ctx context.Context, options v1.ListOptions) (watch.Interface, error) {
+				if tweakListOptions != nil {
+					tweakListOptions(&options)
+				}
+				return client.StorageV1alpha1().SBOMs(namespace).Watch(ctx, options)
 			},
 		},
 		&apistoragev1alpha1.SBOM{},
