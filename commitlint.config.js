e4a58b079b27ce6177a48d65fddceadf2a15fd22
pohanhuangtw <pohan.huang@suse.com>
2025-06-24 04:16:02 +0000

    chore: replace with length with infinity
    Signed-off-by: pohanhuangtw <pohan.huang@suse.com>

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,6 @@
-export default { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'body-max-line-length': [0, 'always', Infinity], // disables the 100-char limit
+  },
+};
