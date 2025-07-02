15303c8688cdeef41e2f07518c8c0c0f98169e51
Fabrizio Sestito <fabrizio.sestito@suse.com>
2025-07-02 11:47:18 +0200

    feat(controller): add VulnerabilityReport reconciler
    Signed-off-by: Fabrizio Sestito <fabrizio.sestito@suse.com>

--- cmd/controller/main.go
+++ cmd/controller/main.go
@@ -213,6 +213,14 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controller.VulnerabilityReportReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "VulnerabilityReport")
+		os.Exit(1)
+	}
+
 	// +kubebuilder:scaffold:builder
 
 	if err = mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
