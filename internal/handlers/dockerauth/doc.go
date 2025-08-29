fc53e7a87ef18d9567a93a4e2a6a70969140e672
Alessio Greggi <alessio.greggi@suse.com>
2025-08-29 09:33:56 +0200

    Update internal/handlers/dockerauth/doc.go
    Co-authored-by: Flavio Castelli <flavio@castelli.me>
    Signed-off-by: Alessio Greggi <alessio.greggi@suse.com>

--- internal/handlers/dockerauth/doc.go
+++ internal/handlers/dockerauth/doc.go
@@ -1,3 +1,4 @@
 // Package dockerauth provides functions to setup Docker authentication
-// for private registries using config.json file.
+// for private registries using the `config.json` file defined
+// inside of a Kubernetes Secret.
 package dockerauth
