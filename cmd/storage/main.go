988cd0b24cc101df160552bbbb1e275555af3acf
Fabrizio Sestito <fabrizio.sestito@suse.com>
2025-09-15 15:19:40 +0200

    refactor(storage): switch to pgsql
    Signed-off-by: Fabrizio Sestito <fabrizio.sestito@suse.com>

--- cmd/storage/main.go
+++ cmd/storage/main.go
@@ -1,33 +1,16 @@
-/*
-Copyright 2016 The Kubernetes Authors.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package main
 
 import (
-	"log"
 	"log/slog"
 	"os"
 
-	"github.com/jmoiron/sqlx"
-	"github.com/rancher/sbombastic/cmd/storage/server"
-	"github.com/rancher/sbombastic/internal/storage"
 	genericapiserver "k8s.io/apiserver/pkg/server"
 	"k8s.io/component-base/cli"
 
-	_ "modernc.org/sqlite"
+	"github.com/jackc/pgx/v5/pgxpool"
+
+	"github.com/rancher/sbombastic/cmd/storage/server"
+	"github.com/rancher/sbombastic/internal/storage"
 )
 
 func main() {
@@ -36,20 +19,38 @@ func main() {
 		Level: slog.LevelDebug,
 	}
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, &opts)).With("component", "storage")
+	ctx := genericapiserver.SetupSignalContext()
 
-	db, err := sqlx.Connect("sqlite", "/data/sqlite/storage.db")
+	dbURI, err := os.ReadFile("/pg/uri")
 	if err != nil {
-		log.Fatalln(err)
+		logger.Error("failed to read database URI", "error", err)
+		os.Exit(1)
 	}
 
-	db.SetMaxOpenConns(1) // Avoid connection pool issues
-	db.MustExec(storage.CreateImageTableSQL)
-	db.MustExec(storage.CreateSBOMTableSQL)
-	db.MustExec(storage.CreateVulnerabilityReportTableSQL)
+	db, err := pgxpool.New(ctx, string(dbURI))
+	if err != nil {
+		logger.Error("failed to create connection pool", "error", err)
+		os.Exit(1)
+	}
 
-	ctx := genericapiserver.SetupSignalContext()
+	// Run migrations
+	if _, err := db.Exec(ctx, storage.CreateImageTableSQL); err != nil {
+		logger.Error("failed to create image table", "error", err)
+		os.Exit(1)
+	}
+	if _, err := db.Exec(ctx, storage.CreateSBOMTableSQL); err != nil {
+		logger.Error("failed to create sbom table", "error", err)
+		os.Exit(1)
+	}
+	if _, err := db.Exec(ctx, storage.CreateVulnerabilityReportTableSQL); err != nil {
+		logger.Error("failed to create vulnerability report table", "error", err)
+		os.Exit(1)
+	}
+
 	options := server.NewWardleServerOptions(db, logger)
 	cmd := server.NewCommandStartWardleServer(ctx, options)
 	code := cli.Run(cmd)
+
+	db.Close()
 	os.Exit(code)
 }
